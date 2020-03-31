8a6f7a9dd35a84881908b3d065b6a3e1ddf04a89
Ian <ian.chen@taiwantaxi.com.tw>
2020-03-31 21:50:50 +0800

    add vue.js, success run.

--- VueJsSample/WebClient/Index.js
+++ VueJsSample/WebClient/Index.js
@@ -1,1 +1,17 @@
-alert("Hello world");
+import Vue from "vue"
+
+new Vue({
+    el: "#app",
+    data: {
+        message: "hello world.",
+        name:"name"
+    },
+    methods: {
+        Hello: function () {
+            alert("hello");
+        }
+    },
+    mounted : function(){
+        console.log('Hello Webpack and Vue !');
+    }
+})
--- VueJsSample/webpack.config.js
+++ VueJsSample/webpack.config.js
@@ -8,4 +8,9 @@ module.exports = {
     },
     mode:"development",
     devtool:"source-map",
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
+    },
 }
