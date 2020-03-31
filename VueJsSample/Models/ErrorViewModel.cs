4e54cf2df96734505fde9e6eb15a28474d905894
Ian <ian.chen@taiwantaxi.com.tw>
2020-03-31 20:49:51 +0800

    init

---
+++ VueJsSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VueJsSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
